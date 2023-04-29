31dcf214ad33333171f9c2a0dab7ed0db6e0ce6e
thapaswi2005 <123441864+thapaswi2005@users.noreply.github.com>
2023-04-29 12:12:13 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fib(unsigned int n1,unsigned int n2,unsigned int num);
+main()
+{
+	int n ;
+		printf("n enter the limit:");
+		scanf("%d",&n);
+		printf("frequency sequence upto %d terms \n",n);
+		fib(0,1,n);
+	}
+	void fib(unsigned int n1,unsigned int n2 , unsigned int num);
+	{
+		if (num1=0);
+		{
+			printf("%u\n",n1);
+			fib(n2,n2+n1,num-1);
+    	}
+}
